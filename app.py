7efdc792365d9c9e7566ec4f054f35f21c91f017
Adwita Arora <adwitaarora2001@gmail.com>
2022-06-20 17:23:19 +0530

    remove db

--- app.py
+++ app.py
@@ -1,35 +1,19 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 import csv
 
-from sqlalchemy import false
 from sentimentAnalysis import analyseEnglish, analyseHindi
 from googletrans import Translator
 
 translator = Translator()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return '<Post %r>' % self.content
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         post_content = request.form['textContent']
-        new_post = Post(content=post_content)
         try:
-            db.session.add(new_post)
-            db.session.commit()
             language = translator.detect(post_content).lang
 
             try:
@@ -44,16 +28,12 @@ def index():
                 prediction = "Could Not Predict"
                 certainity = ""
             post = [post_content, prediction]
-            with open('input_posts.csv', 'a', encoding='UTF8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(post)
-
+    
             return render_template ('index.html', prediction = prediction, certainity = certainity, predicted = False)
         except:
             return "Error Occurred"
     else:
-        posts = Post.query.all()
-        return render_template('index.html', posts=posts, predicted = False)
+        return render_template('index.html', predicted = False)
 
 
 if __name__ == "__main__":
